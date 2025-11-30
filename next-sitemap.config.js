b2d3a5607214be57b714511a3501c57ce9d55cc5
mallela8885-hue <mallela8885@gmail.com>
2025-12-01 01:24:02 +0530

    Create next-sitemap.config.js

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://kranthi-portfilo.vercel.app',
+  generateRobotsTxt: true, // optional
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
+}
